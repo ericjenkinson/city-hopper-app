87a9dfc00927edc5a2199f8462183f675e17d8b4
Eric Jenkinson <eric.jenkinson@ehjconsultancy.com>
2022-08-20 10:59:10 -0500

    Initial Commit

---
+++ CityHopper/CityHopperApp.swift
@@ -0,0 +1,17 @@
+//
+//  CityHopperApp.swift
+//  CityHopper
+//
+//  Created by Eric Jenkinson on 8/20/22.
+//
+
+import SwiftUI
+
+@main
+struct CityHopperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
