1d8e30b8cc5891df898002cefaa09c02def4c07c
Eric Jenkinson <eric.jenkinson@ehjconsultancy.com>
2022-08-20 21:09:39 -0500

    Replaced ContentView with HomeScreen

---
+++ CityHopper/Views/HomeScreen.swift
@@ -0,0 +1,20 @@
+//
+//  HomeScreen.swift
+//  CityHopper
+//
+//  Created by Eric Jenkinson on 8/20/22.
+//
+
+import SwiftUI
+
+struct HomeScreen: View {
+  var body: some View {
+    Text("City Hopper")
+  }
+}
+
+struct HomeScreen_Previews: PreviewProvider {
+  static var previews: some View {
+    HomeScreen()
+  }
+}
