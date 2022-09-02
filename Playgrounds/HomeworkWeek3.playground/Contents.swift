34daf8a38cc220a37826029b2bee2318fd53502b
Eric Jenkinson <eric.jenkinson@ehjconsultancy.com>
2022-09-02 16:40:24 -0500

    Added HomeworkWee3 playground.

---
+++ Playgrounds/HomeworkWeek3.playground/Contents.swift
@@ -0,0 +1,114 @@
+import UIKit
+
+/*
+  Assignment 1:  Function
+  Create a function with 2 parameters: totalAmount and discountPercentage (5%, 10% and so on).
+  Calculate the discountedAmount and subtract this from the totalAmount (using formulae above).
+  Return the totalAmountAfterDiscount.
+ 
+  Materials: https://www.raywenderlich.com/28433240-programming-in-swift-functions-types/lessons/2
+*/
+
+/*
+  Assignment 2: Function
+  Modify the function above to now take in a default discount percentage that will be applied by default to all.
+  Hint: Use function overloading.
+ 
+  Materials: https://www.raywenderlich.com/28433240-programming-in-swift-functions-types/lessons/4
+*/
+
+/*
+ Assignment 3: typealias
+ Create a typealias for a function type that takes in totalAmount and discount type as parameters. It
+ returns the totalAmountAfterDiscount.
+ 
+ Now create a printDiscount function to print the totalAmountAfterDiscount for all discount types. It takes
+ function type (typealias) as a parameter:
+ 
+ Hint: (Double, String) -> Double
+ 
+ Material: https://www.raywenderlich.com/28433240-programming-in-swift-functions-types/lessons/12
+ Typealias are mentioned some in this video.
+*/
+
+/*
+ Assignment 4: Closure
+ Create a closure that calculates the discount. Closures takes parameters as: total amount, discount type and
+ subtracts discounted amount from total amount and prints the total amount after discount.
+
+ Materials: https://www.raywenderlich.com/28433240-programming-in-swift-functions-types/lessons/10
+            https://www.raywenderlich.com/28433240-programming-in-swift-functions-types/lessons/12
+*/
+
+/*
+ Assignment 5: Map
+ Take itemPrices. Let’s say the prices of all of these items are to be increased by 50%.
+ Multiply each element in an array with 0.5. Print the new item price.
+
+ Hint: Use Array's map method.
+ 
+ Materials: https://www.raywenderlich.com/28433240-programming-in-swift-functions-types/lessons/14
+*/
+
+/*
+ Assignment 6: Sorted
+ Create a (Discount types) dictionary (with discount type and percentage amount). Sort this dictionary from
+ highest discount to lowest discount.
+ 
+ Hint: Use Dictionary's sorted
+
+ Materials: https://www.raywenderlich.com/28433240-programming-in-swift-functions-types/lessons/17
+ Note that the materials talk about sorted on array not dictionary, but both are collections.
+*/
+
+/*
+ Assignment 7: Enums and Switch cases
+ Use enums for discount types.
+ Create a function printDiscount to print the value for discount types. Use switch cases and enums to do this.
+
+ Materials: https://www.raywenderlich.com/28433240-programming-in-swift-functions-types/lessons/21
+            https://www.raywenderlich.com/28433240-programming-in-swift-functions-types/lessons/23
+            https://www.raywenderlich.com/28433240-programming-in-swift-functions-types/lessons/24
+ */
+
+/*
+ Assignment 8: Computed property
+ Create a computed property: currentDiscountedAmount that returns the current discounted amount that
+ you apply on the itemPrices.
+ 
+ Materials: https://www.raywenderlich.com/28433240-programming-in-swift-functions-types/lessons/30
+            https://www.raywenderlich.com/28433240-programming-in-swift-functions-types/lessons/35
+*/
+
+/*
+ Assignment 9: Lazy property
+ Create a lazy property that returns the maximum discount that can be applied.
+
+ Materials: https://www.raywenderlich.com/28433240-programming-in-swift-functions-types/lessons/31
+*/
+
+/*
+ Assignment 10: Method
+ Create a method that calculates and returns totalAmountAfterApplyingDicount.
+ 
+ Hint: Method belongs to a class/ struct/ enum.
+ 
+ Materials: https://www.raywenderlich.com/28433240-programming-in-swift-functions-types/lessons/33
+
+*/
+
+/*
+ Assignment 11: Protocol
+ Create a protocol and class conforming to the protocol. For example: Discount that has DiscountType and
+ DiscountPercentage and a method to calculate discount.
+ 
+ Materials: https://www.raywenderlich.com/28433240-programming-in-swift-functions-types/lessons/42
+            
+*/
+
+/*
+ Assignment 12: Extension
+ Create an extension that rounds off the totalDiscountedAmount.
+ 
+ Materials: https://www.raywenderlich.com/28433240-programming-in-swift-functions-types/lessons/43
+*/
