dc7831437b4517fcafbd94ccc6f21c4d0a21cf96
Eric Jenkinson <eric.jenkinson@ehjconsultancy.com>
2022-09-11 13:27:32 -0500

    Fixed issue in which some cities had the incorrect image.

--- CityHopper/Model/Destination.swift
+++ CityHopper/Model/Destination.swift
@@ -19,12 +19,12 @@ class Destination: ObservableObject {
     cities.append(City(name: "Tokyo", image: "imageTokyo", country: "Japan", description: "Samurai", reviews: [Review(id: UUID(), rating: 4.1, description: "Wonderful!")], price: 2400.00))
     
     cities.append(City(name: "Berlin", image: "imageMunich", country: "Germany", description: "Ich bin ein Berliner", reviews: nil, price: 1200.00))
-    cities.append(City(name: "Denver", image: "imageCairo", country: "United States", description: "Mile High!", reviews: [Review(id: UUID(), rating: 4.3, description: "Great!")], price: 1600.00))
+    cities.append(City(name: "Denver", image: "imageDenver", country: "United States", description: "Mile High!", reviews: [Review(id: UUID(), rating: 4.3, description: "Great!")], price: 1600.00))
     cities.append(City(name: "London", image: "imageLondon", country: "United Kingdom", description: "Bangers and Mash!", reviews: [Review(id: UUID(), rating: 3.3, description: "Great!")], price: 1000.00))
     cities.append(City(name: "Los Angeles", image: "imageLosAngeles", country: "United States", description: "Hollywood1", reviews: [Review(id: UUID(), rating: 5.0, description: "Wonderful!")], price: 2000.00))
     cities.append(City(name: "Santorini", image: "imageSantorini", country: "Greece", description: "Blue", reviews: nil, price: 900.00))
     cities.append(City(name: "Paris", image: "imageParis", country: "France", description: "Croissants!", reviews: [Review(id: UUID(), rating: 4.1, description: "Wonderful!")], price: 2400.00))
-    cities.append(City(name: "San Francisco", image: "imageChicago", country: "United States", description: "", reviews: nil, price: 900.00))
+    cities.append(City(name: "San Francisco", image: "imageSanFrancisco", country: "United States", description: "", reviews: nil, price: 900.00))
     cities.append(City(name: "St. Louis", image: "imageStLouis", country: "United State", description: "The Great Arch", reviews: [Review(id: UUID(), rating: 4.1, description: "Wonderful!")], price: 2400.00))
   }
   
