5cab316f43177c02e47900124698c06a689b94ef
Eric Jenkinson <eric.jenkinson@ehjconsultancy.com>
2022-10-13 13:23:31 -0500

    Removed Homework 8 code.

--- CityHopper/Views/HomeScreen/HomeScreen.swift
+++ CityHopper/Views/HomeScreen/HomeScreen.swift
@@ -27,54 +27,8 @@ struct HomeScreen: View {
               }
             }
           }
-    
       }
-      .task {
-        await getData()
-      }
-
   }
-  
-  // methods
-  private func getData(fromURLString urlString: String, completion: @escaping (Result<Data, Error>) -> Void) {
-    if let url = URL(string: urlString) {
-      let urlSession = URLSession(configuration: .default).dataTask(with: url) { (data, response, error) in
-        if let error = error {
-          completion(.failure(error))
-        }
-
-        if let data = data {
-          completion(.success(data))
-        }
-      }
-
-      urlSession.resume()
-    }
-  }
-  
-  private func getData() async {
-    do {
-      let output = try await dataRetriever.getData()
-      
-      let _ = print(output)
-    } catch {
-      print(error)
-    }
-    
-    let output = await dataRetriever.getCookies()
-    let _ = print(output)
-   
-    let urlString = "https://fakestoreapi.com/products"
-    self.getData(fromURLString: urlString) { (result) in
-      switch result {
-      case .success(let data):
-        let _ = print("Bytes from closure \(data)")
-      case .failure(let error):
-        print(error)
-      }
-    }
-  }
-  
 }
 
 struct HomeScreen_Previews: PreviewProvider {
